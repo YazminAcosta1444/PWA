bd0e34fda0021b1801e911dedb57157980be8262
Yazmin Acosta Jimenez <Yazmin.acosta.jimenez@gmail.com>
2024-02-01 23:52:58 -0600

    Commit

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,21 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-        .then((registration) => {
-            console.log('Service Worker registrado con éxito:', registration);
-        })
-        .catch((error) => {
-            console.error('Error al registrar el Service Worker:', error);
-        });
-}
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/icon.png',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
