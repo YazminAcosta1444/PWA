e5f3efa7e5f9a8637e9fe4b6836b078ce9e0a3c5
Yazmin Acosta Jimenez <Yazmin.acosta.jimenez@gmail.com>
2024-02-02 00:28:51 -0600

    Commit

--- service-worker.js
+++ service-worker.js
@@ -3,9 +3,9 @@ self.addEventListener('install', (event) => {
       caches.open('my-cache').then((cache) => {
         return cache.addAll([
           '/',
-          '/index.html',
-          '/manifest.json',
-          '/styles.css',
+          'index.html',
+          'manifest.json',
+          'styles.css',
         ]);
       })
     );
